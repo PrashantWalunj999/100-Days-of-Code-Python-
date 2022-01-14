60671c789ced5f930e4d201d59a38141aca21a76
Walunj (EXT) <ngg4zh@splm.siemens.com>
2022-01-14 21:13:45 +0530

    first commit

---
+++ 100 Days/Day1/day1.py
@@ -0,0 +1,9 @@
+#Day 1 
+
+#Band name Generator
+print("Hi there! Are you confuse about your band name")
+
+name1 = input("Plase enter where you born:\n")
+name2 = input("Plase enter your pet name:\n")
+final_name = name1 + " " + name2
+print("Your band name is ==>\t",final_name)
